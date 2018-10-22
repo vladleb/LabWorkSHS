5e9da40b39d3f01fe63fbb3d15d6467fd219e862
Vlad Lebedev <vladis90@bk.ru>
2018-10-22 10:23:13 +0300

    commit3

--- Lab8Thread/src/test/java/ParallelMatrixProductTest.java
+++ Lab8Thread/src/test/java/ParallelMatrixProductTest.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 import static java.lang.System.err;
 import static java.lang.System.out;
 import static org.junit.Assert.*;
@@ -5,7 +7,7 @@ import static org.junit.Assert.*;
 public class ParallelMatrixProductTest {
 
     @org.junit.Test
-    public void getResult() {
+    public void getResultWithRandMatr() {
         UsualMatrix matrA = new UsualMatrix (3, 5);
         UsualMatrix matrB = new UsualMatrix (5, 3);
         UsualMatrix ansThread = null;
@@ -29,31 +31,70 @@ public class ParallelMatrixProductTest {
         }
         assertArrayEquals (ansThread.getMatrix (), ansOne.getMatrix ());
     }
-//TODO OPTIONAL(JAVA 8), TRY WITH RESOURCES
+
     @org.junit.Test
-    public void multiplyMatrix()
+    public void getResultWithReadyMatr()
     {
-        UsualMatrix matrA = new UsualMatrix (2, 4);
-        UsualMatrix matrB = new UsualMatrix (4, 2);
+        int[][] masMatrixA = { {1,2,3}, {1,2,3}, {1,2,3}};
+        int[][] masMatrixB = { {3,2,1}, {3,2,1}, {3,2,1}};
+        int[][] masMatrixRes = { {18,12,6}, {18,12,6}, {18,12,6}};
+        UsualMatrix matrA = new UsualMatrix (masMatrixA);
+        UsualMatrix matrB = new UsualMatrix (masMatrixB);
         UsualMatrix ansThread = null;
-        UsualMatrix ansOne = null;
         try {
             ParallelMatrixProduct matr = new ParallelMatrixProduct ();
             ansThread = matr.getResult(matrA, matrB, 10);
-            out.println ("Result: " + '\n' + ansThread);
+            out.println (ansThread);
         }
         catch (MatrixException e){
             err.println ( e.getMessage ());
         } catch (Exception e) {
             err.println ( e.getMessage ());
         }
+        assertArrayEquals (ansThread.getMatrix (), masMatrixRes);
+    }
+//TODO OPTIONAL(JAVA 8), TRY WITH RESOURCES
+    @org.junit.Test
+    public void multiplyMatrixWithRandMatr() {
+        UsualMatrix matrA = new UsualMatrix(2, 4);
+        UsualMatrix matrB = new UsualMatrix(4, 2);
+        UsualMatrix ansThread = null;
+        UsualMatrix ansOne = null;
+        try {
+            ParallelMatrixProduct matr = new ParallelMatrixProduct();
+            ansThread = matr.getResult(matrA, matrB, 10);
+            out.println("Result: " + '\n' + ansThread);
+        } catch (MatrixException e) {
+            err.println(e.getMessage());
+        } catch (Exception e) {
+            err.println(e.getMessage());
+        }
+        try {
+            ansOne = new UsualMatrix(ParallelMatrixProduct.multiplyMatrix(matrA, matrB));
+            out.println("Result: " + '\n' + ansOne);
+        } catch (Exception e) {
+            err.println(e.getMessage());
+        }
+        assertArrayEquals(ansThread.getMatrix(), ansOne.getMatrix());
+    }
+
+    @org.junit.Test
+    public void multiplyMatrixWithReadyMatr ()
+    {
+        int[][] masMatrixA = { {1,2,3}, {1,2,3}, {1,2,3}};
+        int[][] masMatrixB = { {3,2,1}, {3,2,1}, {3,2,1}};
+        int[][] masMatrixRes = { {18,12,6}, {18,12,6}, {18,12,6}};
+        UsualMatrix matrA = new UsualMatrix (masMatrixA);
+        UsualMatrix matrB = new UsualMatrix (masMatrixB);
+        UsualMatrix ansOne = null;
         try {
             ansOne = new UsualMatrix (ParallelMatrixProduct.multiplyMatrix (matrA,matrB));
-            out.println ("Result: " + '\n' + ansOne);
+            out.println (ansOne);
         }
         catch (Exception e){
             err.println ( e.getMessage ());
         }
-        assertArrayEquals (ansThread.getMatrix (), ansOne.getMatrix ());
+        assertArrayEquals (ansOne.getMatrix (), masMatrixRes);
+
     }
 }
